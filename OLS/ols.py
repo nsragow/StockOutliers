890fddf04314deee70bb2e7b2e0273905977b5e5
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-06-11 11:14:47 -0400

    edit: nicer plot, show rsquared

--- OLS/ols.py
+++ OLS/ols.py
@@ -21,17 +21,21 @@ def predictions(df,predictors,target,model):
     to_return["cdf"] = culmative
     to_return["resid"] = resids
     to_return = to_return.loc[:,["cdf","resid"]]
-    to_return = to_return.sort_values(by="cdf")
 
 
 
     return to_return,resids.std(),model
 def get_outliers(resid_df,cutoff = .05):
     return resid_df[(resid_df.cdf > (1-cutoff)) | (resid_df.cdf < cutoff)]
-def graph_residuals(resid_df,cutoff = .05):
-    outliers = get_outliers(resid_df,cutoff)
-    norm = resid_df.drop(outliers.index)
-    plt.scatter(y=norm.resid,x=range(norm.shape[0]))
-    plt.scatter(y=outliers.resid,x=range(outliers.shape[0]),color="red")
+def graph_residuals(resid_df,rsquared,cutoff = .05):
+    outliers = get_outliers(resid_df,cutoff).index
+    colors = resid_df.apply(lambda row : "red" if row.name in outliers else "blue",axis=1)
+    fig,ax = plt.subplots()
+
+    ax.set_title(f"rsquared:{rsquared}")
+    print("here")
+    ax.scatter(y=resid_df.resid,x=range(resid_df.shape[0]),color=colors)
+    print("and here")
+    plt.show()
 
 #what is the formula for rsquared? How can I relate it to if a stock is an outlier?
