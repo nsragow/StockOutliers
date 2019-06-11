9969465ffcc1fad38aad68a54ae64f8ade3d7da9
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-06-11 12:55:11 -0400

    fixed bug, set up run func for full run

--- OLS/ols.py
+++ OLS/ols.py
@@ -2,8 +2,17 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 from statsmodels.api import OLS,add_constant
 
+def run(df,target,predictors):
+    target_df = df[target]
+    predictors_df = df[predictors]
+    model = get_model(target_df,predictors_df)
+    resid_df,std = predictions(df,target,predictors,model)
+    outliers = get_outliers(resid_df)
+    graph_residuals(resid_df,model.rsquared)
 
-def get_outliers(target, predictors):
+
+
+def get_model(target, predictors):
     model = OLS(target,add_constant(predictors)).fit()
     return model
 
@@ -17,14 +26,14 @@ def predictions(df,predictors,target,model):
     z_resids = (resids-resids.mean())/resids.std()
 
     culmative = list(map(stats.norm.cdf,z_resids))
-    to_return = df.copy()
-    to_return["cdf"] = culmative
-    to_return["resid"] = resids
-    to_return = to_return.loc[:,["cdf","resid"]]
+    resid_df = df.copy()
+    resid_df["cdf"] = culmative
+    resid_df["resid"] = resids
+    resid_df = resid_df.loc[:,["cdf","resid"]]
 
 
 
-    return to_return,resids.std(),model
+    return resid_df,resids.std()
 def get_outliers(resid_df,cutoff = .05):
     return resid_df[(resid_df.cdf > (1-cutoff)) | (resid_df.cdf < cutoff)]
 def graph_residuals(resid_df,rsquared,cutoff = .05):
