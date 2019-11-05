d02c60dd5d78cf5af72426391733ed61f83d83e8
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-11-05 14:27:09 -0500

    two bugs corrected

--- Yahoo/yahoo_functions.py
+++ Yahoo/yahoo_functions.py
@@ -5,7 +5,7 @@ def ticker_url(ticker: str) -> str:
     ticker = ticker.strip().upper()
     return f"https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}"
 
-def get_html(url: str) -> BeautifulSoup:
+def get_soup(url: str) -> BeautifulSoup:
     response = re.get(url)
     return BeautifulSoup(response.text)
 
@@ -16,14 +16,24 @@ def get_fin_data_table(bs: BeautifulSoup):
     return table_in_list[0]
 
 def get_table_rows(table) -> list:
-    return table.select("div.rw-expnded > div")
+    return table.select("div.rw-expnded > div:nth-child(1)")
 
 def name_and_recent_val(row) -> tuple:
-    title_element = row.select("div[title]")
-    assert len(title_element) == 1
-    title_element = title_element[0]
-    title = title_element.get("title")
-    most_recent_val = row.select("div:nth-child(2) > span")
-    assert len(most_recent_val) == 1
-    val = most_recent_val[0].text
-    return (title,val)
+    try:
+        title_element = row.select("div[title]")
+        assert len(title_element) == 1
+        title_element = title_element[0]
+        title = title_element.get("title")
+        most_recent_val = row.select("div:nth-child(2) > span")
+        assert len(most_recent_val) == 1
+        val = most_recent_val[0].text
+        return (title,val)
+    except AssertionError as ae:
+        return (ValueError("Formatting error" + str(ae)),row)
+
+class YahooTable:
+    def __init__(self, ticker: str):
+        self.url = ticker_url(ticker)
+        self.soup = get_soup(self.url)
+        self.rows = get_table_rows(get_fin_data_table(self.soup))
+        self.names_vals = list(map(name_and_recent_val,self.rows))
